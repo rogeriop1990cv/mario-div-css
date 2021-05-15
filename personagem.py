a61633c40bb67982a50d5319b1cfdfe33e3901d8
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-15 13:43:12 -0300

    gooaba completo

--- personagem.py
+++ personagem.py
@@ -1,15 +1,20 @@
-from io import FileIO
 
 
-with open("goomba.html", "a") as f:
-    dicionario= {
-        1 :  "roxo",
-        2 : "marromm",
-        3 : "pele",
-        4 : "preto"
-    }
-    for linha in range(14):
-        for llinha in range(14):
-            cor = input("digite a cor: ")
-            f.write(f'<div class="{dicionario[cor]}"></div>')
-        f.write("\n")
+lista = [
+ 
+]
+
+
+
+
+dicionario= {
+    1 :  "roxo",
+    2 : "marromm",
+    3 : "pele",
+    4 : "preto"
+}
+
+codigo = open("goomba.html", "a")
+for cor in lista:
+    codigo.write(f'<div class="{dicionario[cor]}"></div>\n')
+codigo.close()
